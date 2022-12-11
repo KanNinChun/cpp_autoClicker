82cda85e5d41c4577514f38d6b662b59ef0fc305
KanNinChun <210226456@stu.vtc.edu.hk>
2022-12-11 17:57:52 +0800

    Added instruction Updated autoclick() code

--- auto clicker/main.cpp
+++ auto clicker/main.cpp
@@ -3,18 +3,24 @@
 
 using namespace std;
 
+void instruction()
+{
+	std::cout << "Press Numpad 1 to Enable auto Left Click  , repress to disable" << std::endl;
+	std::cout << "Press Numpad 2 to Enable auto Right Click , repress to disable" << std::endl;
+}
+
 void autoclick()
 {
 	while (true)
 	{
 		
-		if (GetKeyState(VK_XBUTTON2) & 1)
+		if (GetKeyState(VK_NUMPAD1) & 1)
 		{
 			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
 			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 			Sleep(1);
 		}
-		if (GetKeyState(VK_XBUTTON1) & 1)
+		if (GetKeyState(VK_NUMPAD2) & 1)
 		{
 			mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
 			mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
@@ -25,6 +31,7 @@ void autoclick()
 
 int main()
 {
+	instruction();
 	autoclick();
 	return 0;
 }
